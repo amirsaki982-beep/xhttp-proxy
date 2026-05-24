3b7dc91d14348f61d76fbd77363e80a22a62d57b
amirsaki982-beep <amirsaki982@gmail.com>
2026-05-25 00:36:55 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const net = require('net');
+const url = require('url');
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('OK');
+});
+server.on('connect', (req, clientSocket, head) => {
+  const { hostname, port } = url.parse('http://' + req.url);
+  const serverSocket = net.connect(port || 443, hostname, () => {
+    clientSocket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+    serverSocket.write(head);
+    serverSocket.pipe(clientSocket);
+    clientSocket.pipe(serverSocket);
+  });
+});
+server.listen(PORT);
